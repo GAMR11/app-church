f812f1a3d32788d4033f54a2388cb60959ece259
gamr11 <gamr1308988@gmail.com>
2025-10-11 15:10:53 -0500

    compilado de app

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1060887359"
+    serviceWorkerVersion: "3038659034"
   }
 });
